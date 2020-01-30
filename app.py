1587c17f791aeb05034062e6464285edfd8d0736
Divine Touch <divine.touch07@gmail.com>
2020-01-30 08:15:54 -0600

    set up routes

--- app.py
+++ app.py
@@ -2,8 +2,47 @@ from flask import Flask
 
 app = Flask(__name__)
 
+stores = [
+    {
+        'name': 'My Wonderful Store',
+        'items': [
+            {
+                'name': 'My Item',
+                'price': 15.99
+            }
+        ]
+    }
+]
+
+
 @app.route('/')
 def home():
     return "Hello, world!"
+
+# POST /store data: (name:)
+@app.route('/store', methods=['POST'])
+def create_store():
+    pass
+
+# GET /store/<string:name>
+@app.route('/store/<string:name>')
+def get_store(name):
+    pass
+
+# GET /store
+@app.route('/store')
+def get_store():
+    pass
+
+# POST /store/<string:name>/item (name:, price:)
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_store():
+    pass
+
+# GET /store/<string:name>/item
+@app.route('/store/<string:name>/item')
+def get_items_in_store(name):
+    pass
+
 
 app.run(port=5000)
