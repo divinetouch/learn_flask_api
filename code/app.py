bb8886c2afb490662727043a584e5addaaa585a6
Divine Touch <divine.touch07@gmail.com>
2020-01-31 15:37:58 -0600

    JWT, user authentication

--- code/app.py
+++ code/app.py
@@ -1,31 +1,45 @@
+from flask_jwt import JWT, jwt_required
 from flask import Flask, request
 from flask_restful import Resource, Api
+from dotenv import load_dotenv
+from security import authenticate, identity
+
+import os
+load_dotenv()
 
 app = Flask(__name__)
+app.secret_key = os.getenv('SECRET_KEY')
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity)  # /auth
 
 items = []
 
+
 class Item(Resource):
 
+    @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
         return {'item': item}, 200 if item else 404
 
+    @jwt_required()
     def post(self, name):
         if next(filter(lambda x: x['name'] == name, items), None):
             return {'message': "An item with name '{}' already exists".format(name)}, 400
 
         # force=True mean no need to set the header --> get_json(force=True)
-        data = request.get_json() 
+        data = request.get_json()
 
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
 
+
 class ItemList(Resource):
     def get(self):
         return {'items': items}
+
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
