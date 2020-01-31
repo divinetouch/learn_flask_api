7c389cf513f9f52166ca941ee13e20a04a399c48
Divine Touch <divine.touch07@gmail.com>
2020-01-30 19:38:54 -0600

    set up flask restful

--- code/app.py
+++ code/app.py
@@ -1,12 +1,33 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
-class Student(Resource):
+items = []
+
+class Item(Resource):
+
     def get(self, name):
-        return {'student': name}
+        for item in items:
+            if item['name'] == name:
+                return item
 
+        return {'item': 'null'}, 404
 
-api.add_resource(Student, '/student/<string:name>')
+    def post(self, name):
+        # force=True mean no need to set the header --> get_json(force=True)
+        data = request.get_json() 
+
+        item = {'name': name, 'price': data['price']}
+        items.append(item)
+        return item, 201
+
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
+app.run(port=5000)
