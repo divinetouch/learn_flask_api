e6c866f0fff69994b3eded52576b4486a1264f33
Divine Touch <divine.touch07@gmail.com>
2020-02-07 21:27:19 -0600

    CRUD for item

--- code/create_tables.py
+++ code/create_tables.py
@@ -5,8 +5,15 @@ connection = sqlite3.connect('data.db')
 cursor = connection.cursor()
 
 create_table = 'CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)'
-
 cursor.execute(create_table)
+
+create_table = 'CREATE TABLE IF NOT EXISTS items (name text, price real)'
+cursor.execute(create_table)
+
+cursor.execute('INSERT INTO users VALUES(NULL, "divine", "asdf")')
+cursor.execute('INSERT INTO items VALUES("chair", 10.99)')
+cursor.execute('INSERT INTO items VALUES("table", 20.99)')
+cursor.execute('INSERT INTO items VALUES("mirror", 15.99)')
 
 connection.commit()
 
--- code/item.py
+++ code/item.py
@@ -13,17 +13,31 @@ class Item(Resource):
 
     @jwt_required()
     def get(self, name):
+        item = self.find_by_name(name)
+        if item:
+            return item
+
+        return {'message': 'Item not found'}, 404
+
+    @classmethod
+    def find_by_name(cls, name):
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
 
         query = 'SELECT * from items WHERE name=?'
 
-        cursor.execute(query, name)
+        result = cursor.execute(query, (name, ))
+        row = result.fetchone()
+        connection.close()
+
+        if row:
+            return {'item': {'name': row[0], 'price': row[1]}}
 
     @jwt_required()
     def post(self, name):
 
-        if next(filter(lambda x: x['name'] == name, items), None):
+        if self.find_by_name(name):
+            # not found
             return {'message': "An item with name '{}' already exists".format(name)}, 400
 
         data = Item.parser.parse_args()
@@ -32,29 +46,81 @@ class Item(Resource):
         # data = request.get_json()
 
         item = {'name': name, 'price': data['price']}
-        items.append(item)
+
+        try:
+            self.insert(item)
+        except:
+            # Internal server error
+            return {'message': 'An error occurred inserting the item.'}, 500
+
         return item, 201
+
+    @classmethod
+    def insert(self, item):
+
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = 'INSERT INTO items VALUES (?, ?)'
+        cursor.execute(query, (item['name'], item['price']))
+
+        connection.commit()
+        connection.close
 
     @jwt_required()
     def delete(self, name):
-        global items
-        items = list(filter(lambda x: x['name'] != name, items))
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = 'DELETE FROM items WHERE name=?'
+        cursor.execute(query, (name, ))
+
+        connection.commit()
+        connection.close
+
         return {'message': 'Item deleted'}
 
     @jwt_required()
     def put(self, name):
         data = Item.parser.parse_args()
 
-        item = next(filter(lambda x: x['name'] == name, items), None)
+        item = self.find_by_name(name)
+        updated_item = {'name': name, 'price': data['price']}
         if item is None:
-            item = {'name': name, 'price': data['price']}
-            items.append(item)
+            try:
+                self.insert(updated_item)
+            except:
+                return {'message': 'An error occurred inserting the item.'}, 500
         else:
-            item.update(data)
+            try:
+                self.update(data)
+            except:
+                return {'message': 'An error occurred updating the item.'}, 500
 
         return item
+
+    @classmethod
+    def update(cls, item):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = 'UPDATE items SET price=? WHERE name=?'
+        cursor.execute(query, (item['price'], item['name']))
+
+        connection.commit()
+        connection.close
 
 
 class ItemList(Resource):
     def get(self):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = 'SELECT * from items'
+        result = cursor.execute(query)
+        items = []
+        for row in result:
+            items.append({'name': row[0], 'price': row[1]})
+
+        connection.close
         return {'items': items}
