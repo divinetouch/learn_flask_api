09ef2a886460783e4da02c03639f81eb51cd526f
Divine Touch <divine.touch07@gmail.com>
2020-02-11 15:52:46 -0600

    jwt blacklist

--- code/app.py
+++ code/app.py
@@ -7,6 +7,7 @@ from security import authenticate, identity
 from resources.user import UserRegister, User, UserLogin, TokenRefresh
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
+from resources.blacklist import BLACKLIST
 from db import db
 
 # pylint: disable=invalid-name
@@ -17,6 +18,8 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
+app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
 app.config['JWT_SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.secret_key = os.getenv('SECRET_KEY')
 db.init_app(app)
@@ -52,6 +55,17 @@ def invalid_token_callback(error):
         'description': 'Signature verification failed',
         'error': 'invalid_token'
     }), 401
+
+@jwt.revoked_token_loader
+def reovked_token_callback():
+    return jsonify({
+        'description': 'The token has been revoked',
+        'error': 'token_revoked'
+    }), 401
+
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    return decrypted_token['identity'] in BLACKLIST
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(Store, '/store/<string:name>')
